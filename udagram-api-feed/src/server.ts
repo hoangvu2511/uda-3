df898bbce4a2816d2c7cc9e5885722bc2e0af34f
hoangvu <map1473@gmail.com>
2022-05-03 11:27:34 +0700

    fix bug

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -9,10 +9,10 @@ import { config } from './config/config';
 import { V0_FEED_MODELS } from './controllers/v0/model.index';
 
 (async () => {
-    // await sequelize.addModels(V0_FEED_MODELS);
+    await sequelize.addModels(V0_FEED_MODELS);
 
     console.debug('Initialize database connection...');
-    // await sequelize.sync();
+    await sequelize.sync();
 
     const app = express();
     const port = process.env.PORT || 8080;
--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -10,10 +10,10 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
-  // await sequelize.addModels(V0_USER_MODELS);
+  await sequelize.addModels(V0_USER_MODELS);
 
   console.debug("Initialize database connection...");
-  // await sequelize.sync();
+  await sequelize.sync();
 
   const app = express();
   const port = process.env.PORT || 8080;
